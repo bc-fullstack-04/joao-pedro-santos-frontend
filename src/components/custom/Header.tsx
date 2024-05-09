5a7101036b3b5b7cc6101a9d75a25fbd23946bb9
joaosantosmedeiros <jopesame@gmail.com>
2024-05-09 13:24:13 -0300

    refactor header to a separate component

---
+++ src/components/custom/Header.tsx
@@ -0,0 +1,32 @@
+import { useAuth } from '@/hooks/UseAuth';
+import logo from '../../assets/logo.svg';
+import icon from '../../assets/icon.svg';
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+  const { isAuthenticated } = useAuth();
+
+  return (
+    <header className='flex justify-between py-3 px-16 backdrop-blur-xl backdrop-contrast-50 flex-wrap max-sm:p-1 max-sm:gap-4 max-sm:justify-center max-sm:items-center'>
+        <div className='flex items-center justify-center gap-2'>
+          <img src={logo}  />
+          <span className='text-white'>BootPlay</span>
+        </div>
+        <div className='flex items-center gap-3 '>
+          {isAuthenticated ? 
+            <div className='flex items-center justify-center gap-4 sm:gap-6 md:gap-10 lg:gap-16'>
+              <Link className='flex items-center justify-center text-white text-center h-10 font-semibold rounded-3xl' to={'/login'}>Meus Discos</Link>
+              <Link className='flex items-center justify-center text-white h-10 font-semibold rounded-3xl' to={'/signup'}>Carteira</Link>
+              <Link to={'/dashboard'}><img src={icon}  /></Link>
+            </div>
+          : 
+          <>
+            <Link to={'/login'}><button className='bg-black text-white h-10 px-14 font-semibold rounded-3xl max-sm:px-8'>Entrar</button></Link>
+            <Link to={'/signup'}><button className='bg-[#9EE2FF] text-black h-10 px-14 font-semibold rounded-3xl max-sm:px-8'>Inscrever-se</button></Link>
+          </>
+           }
+          
+        </div>
+      </header>
+  )
+}
--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,19 +1,10 @@
 import { Link } from 'react-router-dom';
-import logo from '../../assets/logo.svg';
+import Header from '@/components/custom/Header';
 
 export default function Landing() {
   return (
     <main className="flex flex-col bg-fundo bg-cover bg-no-repeat h-screen">
-      <header className='flex justify-between py-3 px-16 backdrop-blur-xl backdrop-contrast-50 flex-wrap max-sm:p-1 max-sm:gap-4 max-sm:justify-center max-sm:items-center'>
-        <div className='flex items-center justify-center gap-2'>
-          <img src={logo}  />
-          <span className='text-white'>BootPlay</span>
-        </div>
-        <div className='flex items-center gap-3 '>
-          <Link to={'/login'}><button className='bg-black text-white h-10 px-14 font-semibold rounded-3xl max-sm:px-8'>Entrar</button></Link>
-          <Link to={'/signup'}><button className='bg-[#9EE2FF] text-black h-10 px-14 font-semibold rounded-3xl max-sm:px-8'>Inscrever-se</button></Link>
-        </div>
-      </header>
+      <Header/>
       <section className='flex items-center px-40 h-full backdrop-brightness-50 max-lg:px-4 max-sm:p-2 max-sm:justify-center max-sm:w-auto'>
         <div className='flex flex-col flex-wrap gap-8 max-lg:text-center'>
           <h1 className='text-white text-4xl md:text-5xl lg:text-6xl font-semibold lg:w-[701px]'>A história da música não pode ser esquecida!</h1>
