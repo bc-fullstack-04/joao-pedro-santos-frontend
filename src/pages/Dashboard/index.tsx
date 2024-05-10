e92cd6945f53f3261c86859a89e1d50c1bc8cba6
joaosantosmedeiros <jopesame@gmail.com>
2024-05-10 13:21:02 -0300

    add modal

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -6,6 +6,11 @@ import { AlbumModel } from "@/models/AlbumModel";
 import { Link, useNavigate } from "react-router-dom";
 import Header from "@/components/custom/Header";
 import glass from '../../assets/glass.svg';
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 
 export function Dashboard() {
   const _navegate = useNavigate();
@@ -47,11 +52,27 @@ export function Dashboard() {
             <div className="carousel-home relative left-0 flex items-center">
               {albums.map((album, i) =>(
                 <div className="pr-8 bg-[#19181F]">
-                  <div key={i} style={{'--bg-fundo': `url(${album.images[0].url})`} as React.CSSProperties} className="bg-[image:var(--bg-fundo)] bg-cover bg-no-repeat w-60 h-[245px] rounded-md hover:scale-110 transition">
-                    <div onClick={() => _navegate('')} className="flex h-full justify-center items-center backdrop-brightness-50 p-6 cursor-pointer  hover:scale-105 transition">
-                      <h1 className="text-2xl font-semibold text-center text-white">{album.name}</h1>
-                    </div>
-                  </div>
+                  <Dialog>
+                    <DialogTrigger className="rounded-md">
+                      <div key={i} style={{'--bg-fundo': `url(${album.images[0].url})`} as React.CSSProperties} className="bg-[image:var(--bg-fundo)] bg-cover bg-no-repeat w-60 h-[245px] rounded-md hover:scale-110 transition">
+                        <div onClick={() => _navegate('')} className="flex h-full justify-center items-center backdrop-brightness-50 p-6 cursor-pointer  hover:scale-105 transition">
+                          <h1 className="text-2xl font-semibold text-center text-white">{album.name}</h1>
+                        </div>
+                      </div>
+                      </DialogTrigger>
+                    <DialogContent className="flex gap-0 bg-white p-0 h-[30%] rounded-md">
+                      <img className="rounded-tl-md" src={album.images[0].url} />
+          
+                      <div className="flex flex-col flex-grow px-2 sm:px-4 justify-between mt-10 mb-6 items-center">
+                        <div className="flex flex-col gap-2 mb-3 w-full">
+                          <h2 className="text-center text-sm font-semibold">{album.name}</h2>
+                          <p className="w-full text-sm px-2 sm:px-8">R$ {album.value.toString().replace('.', ',')}</p>
+                          <p className="w-full text-sm px-2 sm:px-8">{album.artists[0].name}</p>
+                        </div>
+                        <button className="bg-[#FBBC05] h-10 w-[90%] rounded-3xl text-white hover:scale-110 transition" >Comprar</button>
+                      </div>
+                    </DialogContent>
+                  </Dialog>
                 </div>
               ))}
             </div>
